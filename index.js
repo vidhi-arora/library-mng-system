2e8536b0c88c87578ce655a5bfece595bd09b7b4
vidhi-arora <73910280+vidhi-arora@users.noreply.github.com>
2022-05-30 18:07:27 +0530

    deploying to heroku

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(userRoutes);
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
 
-    app.use(express.static(path.resolve(__dirname, "client", "build")))
+    app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
