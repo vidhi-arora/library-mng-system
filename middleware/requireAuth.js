9f493318ba7a5e4b0b3c2c0e4cad8bdfd507ecec
vidhi-arora <73910280+vidhi-arora@users.noreply.github.com>
2022-05-30 19:57:47 +0530

    deploying to heroku

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -13,9 +13,14 @@ const secret = process.env.SECRET || 'LIBRARY';
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
+    console.log(req.headers);
+
     res.send(`auth = ${authorization}`);
 
-    if (!authorization) res.status(422).send({ error: "you must be Logged in" });
+    if (!authorization) {
+        console.log("urgh!");
+        res.status(422).send({ error: "you must be Logged in" });
+    }
     try {
         const token = authorization.replace('Bearer ', '');
         jwt.verify(token, secret, async (err, payload) => {
