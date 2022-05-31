a9bc2dcf87021ea85463ae5c543ccbead335fa3a
vidhi-arora <73910280+vidhi-arora@users.noreply.github.com>
2022-05-31 19:23:26 +0530

    changed position of routes

--- index.js
+++ index.js
@@ -29,25 +29,6 @@ const port = process.env.PORT || 3500;
 app.use(bodyparse.json());
 // app.use(cors());    
 
-app.use('/api/login', authroutes);
-app.use('/api/signup', authroutes);
-
-app.use('/api/getuserfine', userRoutes);
-app.use('/api/getissuebooks', userRoutes);
-app.use('/api/issued-books', userRoutes);
-app.use('/api/search/:key', userRoutes);
-app.use('/api/getreturnbooks', userRoutes);
-app.use('/api/searchs/:key/:value', userRoutes);
-app.use('/api/searchby/isbn/:key', userRoutes);
-app.use('/api/delete-issue', userRoutes);
-app.use('/api/request', userRoutes);
-app.use('/api/getrequest', userRoutes);
-app.use('/api/updaterequeststatus', userRoutes);
-app.use('/api/getuserrequests', userRoutes);
-app.use('/api/addbook', userRoutes);
-app.use('/api/deletebook', userRoutes);
-app.use('/api/updatebook', userRoutes);
-
 if (process.env.NODE_ENV === "production") {
 
     app.use(express.static("client/build"));
@@ -58,6 +39,10 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname + "client/build/index.html"));
     });
 }
+
+app.use('/api/', authroutes);
+app.use('/api/', userRoutes);
+
 
 const dbUri = process.env.MONGODB_URI;
 //ADD THE CONNECTION STRING MANUALLY IN dbUri for local development
